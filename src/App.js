29bcb1920bc1868fcbd4fb5b05913c64a40970b8
Bryan MacDiarmid <bryan.macdiarmid@vts.com>
2023-04-23 21:15:30 -0400

    Add uploader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+import { FlatfileButton } from "@flatfile/react";
+
+console.log(process.env)
 export function App() {
-  return <h1>Hello world!</h1>;
+  return <FlatfileButton
+  licenseKey={ process.env.FLATFILE_LICENSE_KEY }
+  customer={{
+    companyId: "ABC-123",
+    companyName: "ABC Corp.",
+    email: "john@abc123.com",
+    name: "John Smith",
+    userId: "12345",
+  }}
+  settings={{
+    type: "Contact",
+    fields: [
+      { label: "Full Name", key: "name" },
+      { label: "Email", key: "email" },
+    ],
+    managed: true,
+  }}
+  onData={async (results) => {
+    // Do something with the data here
+    return "Done!";
+  }}>
+  Import Contacts
+</FlatfileButton>
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env)
+
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
 const container = document.getElementById("app");
 ReactDOM.render(<App />, container);
+
